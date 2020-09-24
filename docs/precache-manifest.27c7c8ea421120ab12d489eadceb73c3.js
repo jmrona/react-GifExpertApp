9967bfbc17a9ff235d5d0e4f4cb1e61956ff538f
Jose Romero <jm_rona@hotmail.com>
2020-09-24 10:05:49 +0100

    docs agregado

---
+++ docs/precache-manifest.27c7c8ea421120ab12d489eadceb73c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41789548ddbd5756617459c44a5c6ea8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "48271986ba290d1c2b6e",
+    "url": "/static/css/main.d654280d.chunk.css"
+  },
+  {
+    "revision": "ccd9761b7db298367323",
+    "url": "/static/js/2.dab6626b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.dab6626b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48271986ba290d1c2b6e",
+    "url": "/static/js/main.d17aabbf.chunk.js"
+  },
+  {
+    "revision": "0a62db85d9147c438712",
+    "url": "/static/js/runtime-main.2bd8def6.js"
+  }
+]);
